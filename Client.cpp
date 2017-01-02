c4c6f201ff77dc94a8d4a679fdfcb630d35b4d02
Della <daniella.fertouk@gmail.com>
2017-01-02 19:48:18 +0200

    update_2.1.17_19:48

--- Client.cpp
+++ Client.cpp
@@ -19,16 +19,23 @@ int main(int argc, char *argv[]) {
     std::list <Trip*> trips;
     //copy of the taxi center in the server without the ability for managing it
     TaxiCenter tc = TaxiCenter(&drivers, &cabs);
+    //buffer for receiving data from the server
     char buffer[1024];
+    //the choice the client should do according to the server
     char choice[2];
     do {
+        //receive a choice from the server
         client.reciveData(choice, sizeof(choice));
         switch (choice[0]) {
-            //create a driver
+            //create drivers and sent them to server
             case '1': {
+                //receive a choice from the server
                 client.reciveData(buffer, sizeof(buffer));
+                //the num of drivers the client should send
                 int numOfDrivers = atoi(buffer);
+                //getting drivers as input and serializing them
                 while (numOfDrivers != 0) {
+                    //creating a driver
                     char dummy;
                     int id;
                     int age;
@@ -37,16 +44,19 @@ int main(int argc, char *argv[]) {
                     int cabId;
                     cin >> id >> dummy >> age >> dummy >> status >> dummy >> exp >> dummy >> cabId;
                     Driver *d = new Driver(id, age, status, exp, cabId);
+                    //serialize driver
                     string str = boost::lexical_cast<string>(id) + "," + boost::lexical_cast<string>(age) + "," +
                                  status + "," + boost::lexical_cast<string>(exp) + "," +
                                  boost::lexical_cast<string>(cabId);
                     drivers.push_back(d);
+                    //send a driver to the server
                     client.sendData(str);
                     numOfDrivers--;
                 }
                 //receive the num of cabs he will get
                 client.reciveData(buffer, sizeof(buffer));
                 int numOfCabs = atoi(buffer);
+                //getting cabs and deserializing them
                 while (numOfCabs != 0) {
                     client.reciveData(buffer, sizeof(buffer));
                     char *cab[4];
@@ -58,6 +68,7 @@ int main(int argc, char *argv[]) {
                         i++;
                         split = strtok(NULL, ",");
                     }
+                    //creating a cab
                     if (*cab[1] == '1') {
                         StandardCab *c = new StandardCab(atoi(cab[0]), atoi(cab[1]), *cab[2], *cab[3]);
                         c->setLocation(Point(0, 0));
@@ -70,6 +81,7 @@ int main(int argc, char *argv[]) {
                     }
                     numOfCabs--;
                 }
+                //assign cabs to drivers
                 tc.assignCabsToDrivers();
                 break;
             }
@@ -79,13 +91,17 @@ int main(int argc, char *argv[]) {
                 //all drivers are driving
             case '6':
                 break;
+            //assign trips to drivers and advance them
             case '9':
+                //assign trips to drivers
                 if (trips.empty()) {
+                    //receive a choice from the server
                     client.reciveData(buffer, sizeof(buffer));
                     char *trip[9];
                     int i = 0;
                     char *split;
                     split = strtok(buffer, ",");
+                    //desrializing trips
                     while (split != NULL && i < 9) {
                         trip[i] = split;
                         i++;
@@ -93,6 +109,7 @@ int main(int argc, char *argv[]) {
                     }
                     list<Cab *>::iterator cabsIteratorStart = cabs.begin();
                     list<Cab *>::iterator cabsIteratorEnd = cabs.end();
+                    //assign trips to cabs
                     while (cabsIteratorStart != cabsIteratorEnd) {
                         if ((*cabsIteratorStart)->getId() == atoi(trip[0])) {
                             (*cabsIteratorStart)->setTrip(new Trip(atoi(trip[1]), Point(atoi(trip[2]), atoi(trip[3])),
@@ -102,8 +119,11 @@ int main(int argc, char *argv[]) {
                         }
                         cabsIteratorStart++;
                     }
+                    //we advance drivers in their trips
                 } else {
+                    //receive a new location
                     client.reciveData(buffer, sizeof(buffer));
+                    //desrialize a new location of driver
                     char *point[3];
                     int i = 0;
                     char *split;
@@ -115,6 +135,7 @@ int main(int argc, char *argv[]) {
                     }
                     list<Cab *>::iterator cabsIteratorStart = cabs.begin();
                     list<Cab *>::iterator cabsIteratorEnd = cabs.end();
+                    //update the location of the cabs after advance
                     while (cabsIteratorStart != cabsIteratorEnd) {
                         if ((*cabsIteratorStart)->getId() == atoi(point[0])) {
                             (*cabsIteratorStart)->setLocation(Point(atoi(point[1]), atoi(point[2])));
@@ -126,7 +147,9 @@ int main(int argc, char *argv[]) {
             default:
                 break;
         }
+        //'7' should end the communication between the client and the server
     } while (choice[0] != '7');
+    //clearing all the data
     drivers.clear();
     cabs.clear();
     trips.clear();
